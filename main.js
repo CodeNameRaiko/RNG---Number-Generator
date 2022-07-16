3432169898b0b51e1ff9dda2e8615eddbdbf0f64
[CD] Raiko <64981218+CodeNameRaiko@users.noreply.github.com>
2022-07-16 17:36:29 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,50 @@
 var startButton = document.getElementById('start')
 var alignmentBtn = document.getElementById('align')
 var divOne = document.getElementById('pageTwo')
-var amount = document.getElementById('amount')
 var OkButton = document.getElementById('submit-btn')
-var bgSVG = document.getElementById('bg-svg')
+var bgSVG = document.getElementById('res-svg')
 var list = document.getElementById('list') 
 
+// Startpage -> RNG Pannel
 startButton.addEventListener('click', GetStarted)
 function GetStarted() {
-   startButton.classList.add('hide');
-   alignmentBtn.classList.add('hide');
+  startButton.classList.add('hide');
+  alignmentBtn.classList.add('hide');
    divOne.classList.remove('hidehide');
-}
-
+}  
+//RNG Pannel -> results
 OkButton.addEventListener('click', resultsPage)
 function resultsPage() {
   bgSVG.classList.remove('bg-svg')
   bgSVG.classList.add('container')
 }
 
-OkButton.addEventListener('click', results)
-function results() {
+// Amount of 'results()' repeats
+OkButton.addEventListener('click', rngRepeat) 
+function rngRepeat() {
+  const amount = document.querySelector('#amount').value 
+  
+  for (let i = 0; i < parseInt(amount); i++)
+  results();   
+}
+ 
+//Results  
+/*OkButton.addEventListener('click', results) */
+
+ function results() {
+   //part 1
   const from = document.querySelector("#from").value;
   const to = document.querySelector("#to").value;
   let createdNum = parseInt(Math.floor(Math.random() * parseInt(to)));
+  //pat 2
   if( createdNum >= parseInt(from) ) {
     var listItem = document.createElement('p') 
     listItem.innerText = parseInt(createdNum); 
     listItem.classList.add('numbers')
-  }
+  }  
+  //part 3
   var styleDiv = document.createElement('div')
   list.appendChild(styleDiv)
   styleDiv.appendChild(listItem)
   styleDiv.classList.add('styleDiv') 
 } 
-
-
-
-
