b32fec9c811a337223df13c4b883d205841783a3
[CD] Raiko <64981218+CodeNameRaiko@users.noreply.github.com>
2022-07-16 16:43:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 var startButton = document.getElementById('start')
 var alignmentBtn = document.getElementById('align')
 var divOne = document.getElementById('pageTwo')
-var amount = document.getElementById('amount')
 var OkButton = document.getElementById('submit-btn')
-var bgSVG = document.getElementById('bg-svg')
+var bgSVG = document.getElementById('res-svg')
 var list = document.getElementById('list') 
 
+// Startpage -> RNG Pannel
 startButton.addEventListener('click', GetStarted)
 function GetStarted() {
    startButton.classList.add('hide');
    alignmentBtn.classList.add('hide');
    divOne.classList.remove('hidehide');
 }
-
+//RNG Pannel -> results
 OkButton.addEventListener('click', resultsPage)
 function resultsPage() {
   bgSVG.classList.remove('bg-svg')
   bgSVG.classList.add('container')
 }
-
+//Results 
 OkButton.addEventListener('click', results)
 function results() {
   const from = document.querySelector("#from").value;
@@ -34,7 +34,19 @@ function results() {
   styleDiv.appendChild(listItem)
   styleDiv.classList.add('styleDiv') 
 } 
+// Amount of 'results()' repeats
+OkButton.addEventListener('click', rngRepeat) 
 
 
+
+
+
+ 
+function rngRepeat() {
+  const amount = document.querySelector('#amount').value 
+  
+  for (let i = 0; i < parseInt(amount); i++)
+    results();  
+}
 
 
